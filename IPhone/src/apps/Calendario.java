f92f816d490b4bd235ff755db3b71ce636c9cae8
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-19 18:18:51 -0300

    Finalizado Calendario, aumente a tela do terminal para visualizá-lo

--- IPhone/src/apps/Calendario.java
+++ IPhone/src/apps/Calendario.java
@@ -1,43 +1,50 @@
 package apps;
 
+import appabstract.App;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-import appabstract.App;
-
 public class Calendario extends App {
     @Override
     public void abrir() {
-        LocalDate hoje = LocalDate.now();
-        LocalDate primeiroDiaDoMes = hoje.withDayOfMonth(1);
+        System.out.println("Abrindo calendário...");
 
-        String mesAno = hoje.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + " " + hoje.getYear();
-        System.out.println("Calendário - " + mesAno);
+        try {
+            LocalDate hoje = LocalDate.now();
+            LocalDate primeiroDiaDoMes = hoje.withDayOfMonth(1);
 
-        String[] diasDaSemana = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"};
-        for (String dia : diasDaSemana) {
-            System.out.print(dia + "\t");
-        }
-        System.out.println();
+            String mesAno = hoje.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt", "BR")) + " " + hoje.getYear();
+            System.out.println("Calendário - " + mesAno);
 
-        int primeiroDiaSemana = primeiroDiaDoMes.getDayOfWeek().getValue() % 7; // Ajuste para começar no domingo
-        for (int i = 0; i < primeiroDiaSemana; i++) {
-            System.out.print("\t");
-        }
-
-        int ultimoDiaDoMes = hoje.lengthOfMonth();
-        for (int dia = 1; dia <= ultimoDiaDoMes; dia++) {
-            if (dia == hoje.getDayOfMonth()) {
-                System.out.printf("[%d]\t", dia); // Marca o dia atual
-            } else {
+            String[] diasDaSemana = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"};
+            for (String dia : diasDaSemana) {
                 System.out.print(dia + "\t");
             }
+            System.out.println();
 
-            if ((dia + primeiroDiaSemana) % 7 == 0) {
-                System.out.println();
+            int primeiroDiaSemana = primeiroDiaDoMes.getDayOfWeek().getValue() % 7; // Ajuste para começar no domingo
+            for (int i = 0; i < primeiroDiaSemana; i++) {
+                System.out.print("\t");
             }
+
+            int ultimoDiaDoMes = hoje.lengthOfMonth();
+            for (int dia = 1; dia <= ultimoDiaDoMes; dia++) {
+                if (dia == hoje.getDayOfMonth()) {
+                    System.out.printf("[%d]\t", dia); // Marca o dia atual
+                } else {
+                    System.out.print(dia + "\t");
+                }
+
+                if ((dia + primeiroDiaSemana) % 7 == 0) {
+                    System.out.println();
+                }
+            }
+            System.out.println();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println();
+
+        System.out.println("Calendário aberto.");
     }
 }
